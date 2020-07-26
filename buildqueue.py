78ccf1dfe1a22d71cd4809c4424ec26b33e5bce1
eine <eine@users.noreply.github.com>
2020-07-26 20:13:44 +0200

    filter queue to skip packages already available in 'staging'

--- buildqueue.py
+++ buildqueue.py
@@ -1,12 +1,12 @@
+from os import environ
+from github import Github
 from urllib.request import urlopen
 from json import loads
 from pathlib import Path
 from subprocess import check_call
 from sys import stdout
+import fnmatch
 import pytest
-
-PKGEXT='.pkg.tar.zst'
-SRCEXT='.src.tar.gz'
 
 ROOT = Path(__file__).resolve().parent
 
@@ -39,7 +39,7 @@ def test_file(pkg):
             '--skippgpcheck',
             '--allsource'
         ] + ([] if isMSYS else ['--config', '/etc/makepkg_mingw64.conf']))
-        run_cmd(['mv', '*%s' % PKGEXT, '*%s' % SRCEXT, '../../msys2-devtools/assets/'])
+        run_cmd(['mv', '*.pkg.tar.*', '*.src.tar.gz', '../../msys2-devtools/assets/'])
         print('::endgroup::')
         stdout.flush()
     except:
@@ -48,7 +48,39 @@ def test_file(pkg):
         pytest.fail()
 
 def pytest_generate_tests(metafunc):
+    gh = Github(environ["GITHUB_TOKEN"])
+    #gh = Github(environ["GITHUB_USER"], environ["GITHUB_PASS"])
+
+    assets = [
+        asset.name
+        for asset in gh.get_repo('msys2/msys2-devtools').get_release('staging').get_assets()
+    ]
+
+    tasks = []
+    done = []
+
+    for pkg in loads(urlopen("https://packages.msys2.org/api/buildqueue").read()):
+        isMSYS = pkg['repo_url'].split('/')[-1][0:5] == 'MSYS2'
+        isDone = True
+        for item in pkg['packages']:
+            if not fnmatch.filter(assets, '%s-%s-*.pkg.tar.*' % (
+                item,
+                pkg['version']
+            )):
+                isDone = False
+                break
+        if isDone:
+            done.append(pkg)
+        else:
+            tasks.append(pkg)
+
+    print("\nSKIPPED packages; already in 'staging':")
+    for pkg in done:
+        print(" -", pkg['repo_path'])
+
+    # Packages that take too long to build, and should be handled manually
+    skip = ['mingw-w64-clang']
+
     metafunc.parametrize("pkg", [
-        pkg for pkg in loads(urlopen("https://packages.msys2.org/api/buildqueue").read())
-        if pkg['repo_path'] not in ('mingw-w64-clang')
+        pkg for pkg in tasks if pkg['repo_path'] not in skip
     ])
