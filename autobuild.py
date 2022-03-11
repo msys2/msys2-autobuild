1e254ee060d2fc1aec879e898ea5a82527c3e201
Christoph Reiter <reiter.christoph@gmail.com>
2022-03-11 15:50:06 +0100

    Another optional dep

--- autobuild.py
+++ autobuild.py
@@ -91,6 +91,7 @@ class Config:
 
     OPTIONAL_DEPS: Dict[str, List[str]] = {
         "mingw-w64-headers-git": ["mingw-w64-winpthreads-git", "mingw-w64-tools-git"],
+        "mingw-w64-crt-git": ["mingw-w64-winpthreads-git"],
     }
     """XXX: In case of cycles we mark these deps as optional"""
 
