bb54adc298d793e0819881117b688d5adfa0f2e3
Christoph Reiter <reiter.christoph@gmail.com>
2025-03-07 11:41:06 +0100

    Add verbosity option and write logs to stderr by default
    default is warning, -v means info, -vv means debug

--- msys2_autobuild/main.py
+++ msys2_autobuild/main.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import logging
 from typing import List
 
 from . import (cmd_build, cmd_clean_assets, cmd_clear_failed, cmd_fetch_assets,
@@ -10,9 +11,14 @@ from .utils import install_requests_cache
 
 def main(argv: List[str]) -> None:
     parser = argparse.ArgumentParser(description="Build packages", allow_abbrev=False)
+    parser.add_argument(
+        '-v', '--verbose',
+        action='count',
+        default=0,
+        help='Increase verbosity (can be used multiple times)'
+    )
     parser.set_defaults(func=lambda *x: parser.print_help())
     subparsers = parser.add_subparsers(title="subcommands")
-
     cmd_build.add_parser(subparsers)
     cmd_show_build.add_parser(subparsers)
     cmd_write_build_plan.add_parser(subparsers)
@@ -23,6 +29,13 @@ def main(argv: List[str]) -> None:
     cmd_clean_assets.add_parser(subparsers)
 
     args = parser.parse_args(argv[1:])
+    level_map = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+    logging.basicConfig(
+        level=level_map.get(args.verbose, logging.DEBUG),
+        handlers=[logging.StreamHandler(sys.stderr)],
+        format='[%(asctime)s] [%(levelname)8s] [%(name)s:%(module)s:%(lineno)d] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
     with install_requests_cache():
         args.func(args)
 
