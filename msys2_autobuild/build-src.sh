339bf23cfa0a2b51a8dd7a2f2b4918628e2e24e0
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-24 23:18:02 +0200

    build-src: use PACMAN env var if available
    We override pacman in makepkg with it, so we should do the same
    in our custom build script. Otherwise our custom pacman config is not
    active.

--- msys2_autobuild/build-src.sh
+++ msys2_autobuild/build-src.sh
@@ -13,26 +13,27 @@ fi
 # XXX: in theory we need "depends" as well, but this seems good enough for now.
 srcinfo=$("$1" --printsrcinfo)
 mapfile -t deps < <(awk '$1 == "makedepends" { print $3 }' <<< "$srcinfo")
+pacman=${PACMAN:-pacman}
 
 if ((${#deps[@]})); then
-	before=$(pacman --query --quiet)
+	before=$("$pacman" --query --quiet)
 
 	cleanup() {
 		status=$?
-		after=$(pacman --query --quiet) || exit 1
+		after=$("$pacman" --query --quiet) || exit 1
 
 		mapfile -t added < <(grep --invert-match --line-regexp --fixed-strings \
 			--file <(printf '%s\n' "$before") <<< "$after")
 		if ((${#added[@]})); then
 			echo "Dependencies to be removed: ${added[*]}"
-			pacman --remove --nosave --unneeded --noconfirm --noprogressbar "${added[@]}" || exit 1
+			"$pacman" --remove --nosave --unneeded --noconfirm --noprogressbar "${added[@]}" || exit 1
 		fi
 		exit "$status"
 	}
 	trap cleanup EXIT
 
 	echo "Dependencies to be installed: ${deps[*]}"
-	pacman --sync --needed --asdeps --noconfirm --noprogressbar "${deps[@]}"
+	"$pacman" --sync --needed --asdeps --noconfirm --noprogressbar "${deps[@]}"
 fi
 
 "$@"
